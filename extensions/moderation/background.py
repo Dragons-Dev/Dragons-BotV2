d9b4972ba1cc92500e44366bb23c1f898ec35c71
DTheIcyDragon <74721808+DTheIcyDragon@users.noreply.github.com>
2024-02-21 22:06:44 +0100

    feat: ✨add caching to webhooks (#24)

--- extensions/moderation/background.py
+++ extensions/moderation/background.py
@@ -1,5 +1,8 @@
+from typing import no_type_check
+
 import discord
 from discord.ext import commands
+from discord.utils import get_or_fetch
 
 from utils import Bot, CustomLogger
 from utils.enums import SettingsEnum
@@ -9,23 +12,29 @@ class ModBackground(commands.Cog):
     def __init__(self, client):
         self.client: Bot = client
         self.logger = CustomLogger(self.qualified_name, self.client.boot_time)
+        self.audit_webhooks = {}
 
+    @no_type_check  # mypy isn't really happy with this so duck you mypy
     @commands.Cog.listener("on_audit_log_entry")
     async def audit_listener(self, entry: discord.AuditLogEntry):
         audit_channel = await self.client.db.get_setting(SettingsEnum.AuditLogChannel, entry.guild)
-        audit_channel = self.client.get_channel(audit_channel)
+        audit_channel = await get_or_fetch(obj=entry.guild, attr="channel", id=audit_channel, default=None)
+        try:
+            audit_webhook = self.audit_webhooks[f"{audit_channel.guild.id}"]
+        except KeyError:
+            webhooks = await audit_channel.webhooks()
+            for webhook in webhooks:
+                if webhook.name == "Audit-Webhook":
+                    self.audit_webhooks[f"{audit_channel.guild.id}"] = webhook
+                    audit_webhook = webhook
+                    break
+            else:
+                audit_webhook = await audit_channel.create_webhook(
+                    name="Audit-Webhook", reason="Creating Audit-Webhook"
+                )
+                self.audit_webhooks[f"{audit_channel.guild.id}"] = audit_webhook
 
-        webhooks = await audit_channel.webhooks()  # type: ignore
-        for webhook in webhooks:
-            if webhook.name == "Audit-Webhook":
-                hook = webhook
-                break
-        else:
-            hook = await audit_channel.create_webhook(  # type: ignore
-                name="Audit-Webhook", reason="Creating Audit-Webhook"
-            )
-
-        await hook.send(
+        await audit_webhook.send(
             content=entry.action,
             username=f"{self.client.user.name} Log",
             avatar_url=self.client.user.avatar.url,
