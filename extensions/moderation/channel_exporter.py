66d336db8aaddbb2b71e32e45d9e767772731812
paul <paulnicolai.hennings@gmail.com>
2024-03-31 10:36:54 +0200

    fix: 🔒 export command now only for mod-role

--- extensions/moderation/channel_exporter.py
+++ extensions/moderation/channel_exporter.py
@@ -6,7 +6,7 @@ import discord
 import pycord.multicog as pycog
 from discord.ext import commands
 
-from utils import Bot, CustomLogger
+from utils import Bot, CustomLogger, is_team
 
 
 class ChannelExporter(commands.Cog):
@@ -15,9 +15,10 @@ class ChannelExporter(commands.Cog):
         self.logger = CustomLogger(self.qualified_name, self.client.boot_time)
 
     @pycog.subcommand("mod", independent=True)
+    @is_team()
     @commands.slash_command(name="export", description="Exports a channel to a single html file to archive it.")
     async def export(self, ctx: discord.ApplicationContext):
-        first_resp = await ctx.response.defer(ephemeral=True, invisible=False)
+        await ctx.response.defer(ephemeral=True, invisible=False)
         transscript = await chat_exporter.export(
             channel=ctx.channel,
             guild=ctx.guild,
