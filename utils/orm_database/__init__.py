6ae2f410167161984915dfa5f63761aa943bdfa6
K4esus <nigrambow@gmail.com>
2026-03-10 19:40:35 +0100

    init update um pre commit fehler zu fixen

--- utils/orm_database/__init__.py
+++ utils/orm_database/__init__.py
@@ -1,2 +1,4 @@
 from .database import ORMDataBase
-from .models import *
+from .models import Base, Settings, Infractions, Join2Create, Modmail, UserStats, BotStatus, EnabledCommands, Events, Confirmation
+
+__all__ = [ORMDataBase, Base, Settings, Infractions, Join2Create, Modmail, UserStats, BotStatus, EnabledCommands, Events, Confirmation]
